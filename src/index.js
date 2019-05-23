0c77440c9c838e04068898054e4527afca293965
nagerz <znager@gmail.com>
2019-05-23 10:08:14 -0600

    Adds site shows top word

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  showTopWord();
+  var button = document.getElementById("button");
+  button.addEventListener("click", function(){
+    addWord();
+  })
 })
+
+function showTopWord(){
+  fetch("https://wordwatch-api.herokuapp.com//api/v1/top_word")
+  .then(response => {
+    return response.json()
+  })
+  .then(result => {
+    var topWord = Object.keys(result.word)[0]
+    var count = result.word[topWord]
+    return document.getElementById("word-count").innerHTML = `${topWord}: (${count})`;
+  })
+};
+
+function addWord(){
+  var newWord = document.getElementById("textarea").innerHTML;
+  fetch("https://wordwatch-api.herokuapp.com/api/v1/words",{
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      word:{ value: `${newWord}`}
+    })
+  })
+};
